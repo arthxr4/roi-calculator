676f7b39acf906f3deac5b72b87d0f08608a577f
Arthur <arthurlagier@MacBook-Pro-de-Arthur.local>
2025-02-19 14:11:39 +0100

    Mise à jour avec la police Inter

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        instrument: ["Inter", "sans-serif"],
+      },
     },
   },
   plugins: [],
